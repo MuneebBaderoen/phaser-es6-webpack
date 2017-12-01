3e2702b53f16248756c342ee8199ea5b5ed8d947
Muneeb Baderoen <mbaderoen@gmail.com>
2017-12-01 16:45:45 +0200

    Remove unused boilerplate files

--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@ import 'pixi'
 import 'p2'
 import Phaser from 'phaser'
 
-import BootState from './states/Boot'
 import { InitialState } from './states/Initial'
-import GameState from './states/Game'
 
 import config from './config'
 
@@ -17,7 +15,7 @@ class Game extends Phaser.Game {
     config.gameHeight = docElement.clientHeight
 
     // Initialize webgl context
-    super(config.gameWidth, config.gameHeight, Phaser.WEBGL, 'content', null)
+    super(config.gameWidth, config.gameHeight, Phaser.AUTO, 'content', null)
 
     // this.state.add('Boot', BootState, false)
     this.state.add('Game', InitialState, false)
